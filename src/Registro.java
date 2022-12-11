da260ccb8ad13e812d5b0f74ba6870715499edd5
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2022-12-10 22:19:29 -0300

    metodos binarios en el Registro

--- src/Registro.java
+++ src/Registro.java
@@ -18,8 +18,39 @@ public class Registro {
     public String getId(){
         return id;
     }
+
+    /* 
+     * Utilizamos este metodo con el fin de obtener el binario que utilizaremos
+     * para algunas operaciones
+     */
+    public String getBinarioValor(){
+        String binario= Integer.toBinaryString(valor);
+        int largoBinario = binario.length();
+        if(largoBinario==16){
+            return binario;
+        }else{
+            String binarioCorrecto="";
+            for(int i=0;i<(16-largoBinario);i++){
+                binarioCorrecto+="0";
+            }
+            binarioCorrecto+=binario;
+            return binarioCorrecto;
+        }
+    }
+
+    public void setBinarioValor(String binario){
+        valor=Integer.parseInt(binario,2);
+    }
     
     public String mostrarRegistro(){
         return "[id: "+id+" ; "+"valor: "+valor+"]";
+    }
+
+    public static void main(String[] args) {
+        Registro r1=new Registro("rs1", 0);
+        System.out.println(r1.getBinarioValor()+r1.mostrarRegistro());
+
+        r1.setBinarioValor("101");
+        System.out.println(r1.getBinarioValor()+r1.mostrarRegistro());
     }
 }

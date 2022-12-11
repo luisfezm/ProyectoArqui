33e2aca3e964ec42a4cf2665256afbf3c48fab5e
Bastián Núñez <bastinuez@hotmail.com>
2022-12-11 19:53:36 -0300

    Nuevas operaciones
    se añade, lsl,lsr y asr

--- src/Arquitectura.java
+++ src/Arquitectura.java
@@ -143,10 +143,10 @@ public class Arquitectura {
                 break;
             case "and":
                 if (instruccion.registroSecundario == "") {
-                        operacionAnd( // con imm
-                                instruccion.registroDestino,
-                                instruccion.registroPrincipal,
-                                instruccion.valorImm);
+                    operacionAnd( // con imm
+                            instruccion.registroDestino,
+                            instruccion.registroPrincipal,
+                            instruccion.valorImm);
                 } else {
                     operacionAnd( // en caso contrario, es con registro
                             instruccion.registroDestino,
@@ -188,6 +188,30 @@ public class Arquitectura {
                     operacionMov(
                             instruccion.registroDestino,
                             instruccion.registroPrincipal);
+                }
+                break;
+            case "lsl":
+                if (instruccion.registroSecundario == "") {
+                    operacionLsl(instruccion.registroDestino, instruccion.registroPrincipal, instruccion.valorImm);
+                } else {
+                    operacionLsl(instruccion.registroDestino, instruccion.registroPrincipal,
+                            instruccion.registroSecundario);
+                }
+                break;
+            case "lsr":
+                if (instruccion.registroSecundario == "") {
+                    operacionLsr(instruccion.registroDestino, instruccion.registroPrincipal, instruccion.valorImm);
+                } else {
+                    operacionLsr(instruccion.registroDestino, instruccion.registroPrincipal,
+                            instruccion.registroSecundario);
+                }
+                break;
+            case "asr":
+                if (instruccion.registroSecundario == "") {
+                    operacionAsr(instruccion.registroDestino, instruccion.registroPrincipal, instruccion.valorImm);
+                } else {
+                    operacionAsr(instruccion.registroDestino, instruccion.registroPrincipal,
+                            instruccion.registroSecundario);
                 }
                 break;
         }
@@ -249,86 +273,87 @@ public class Arquitectura {
     }
 
     public void operacionAnd(String registroDestino, String registroPrincipal, String registroSecundario) {
-        String binarioPrin=getBinarioValorRegistro(registroPrincipal);
-        String binarioSec=getBinarioValorRegistro(registroSecundario);
-        String binarioResultante="";
-        for(int i=0;i<16;i++){
-            if(binarioPrin.charAt(i)=='1' && binarioSec.charAt(i)=='1'){
-                binarioResultante+="1";
-            }else{
-                binarioResultante+="0";
+        String binarioPrin = getBinarioValorRegistro(registroPrincipal);
+        String binarioSec = getBinarioValorRegistro(registroSecundario);
+        String binarioResultante = "";
+        for (int i = 0; i < 16; i++) {
+            if (binarioPrin.charAt(i) == '1' && binarioSec.charAt(i) == '1') {
+                binarioResultante += "1";
+            } else {
+                binarioResultante += "0";
             }
         }
         setBinarioValorRegistro(registroDestino, binarioResultante);
     }
 
     public void operacionAnd(String registroDestino, String registroPrincipal, int valorImm) {
-        String binarioPrin=getBinarioValorRegistro(registroPrincipal);
-        String binarioImm=Integer.toBinaryString(valorImm);
-        binarioImm=completarBinario16B(binarioImm);
-        String binarioResultante="";
-        for(int i=0;i<16;i++){
-            if(binarioPrin.charAt(i)=='1' && binarioImm.charAt(i)=='1'){
-                binarioResultante+="1";
-            }else{
-                binarioResultante+="0";
+        String binarioPrin = getBinarioValorRegistro(registroPrincipal);
+        String binarioImm = Integer.toBinaryString(valorImm);
+        binarioImm = completarBinario16B(binarioImm);
+        String binarioResultante = "";
+        for (int i = 0; i < 16; i++) {
+            if (binarioPrin.charAt(i) == '1' && binarioImm.charAt(i) == '1') {
+                binarioResultante += "1";
+            } else {
+                binarioResultante += "0";
             }
         }
         setBinarioValorRegistro(registroDestino, binarioResultante);
     }
 
     public void operacionOr(String registroDestino, String registroPrincipal, String registroSecundario) {
-        String binarioPrin=getBinarioValorRegistro(registroPrincipal);
-        String binarioSec=getBinarioValorRegistro(registroSecundario);
-        String binarioResultante="";
-        for(int i=0;i<16;i++){
-            if(binarioPrin.charAt(i)=='1' || binarioSec.charAt(i)=='1'){
-                binarioResultante+="1";
-            }else{
-                binarioResultante+="0";
+        String binarioPrin = getBinarioValorRegistro(registroPrincipal);
+        String binarioSec = getBinarioValorRegistro(registroSecundario);
+        String binarioResultante = "";
+        for (int i = 0; i < 16; i++) {
+            if (binarioPrin.charAt(i) == '1' || binarioSec.charAt(i) == '1') {
+                binarioResultante += "1";
+            } else {
+                binarioResultante += "0";
             }
         }
         setBinarioValorRegistro(registroDestino, binarioResultante);
     }
 
     public void operacionOr(String registroDestino, String registroPrincipal, int valorImm) {
-        String binarioPrin=getBinarioValorRegistro(registroPrincipal);
-        String binarioImm=Integer.toBinaryString(valorImm);
-        binarioImm=completarBinario16B(binarioImm);
-        String binarioResultante="";
-        for(int i=0;i<16;i++){
-            if(binarioPrin.charAt(i)=='1' || binarioImm.charAt(i)=='1'){
-                binarioResultante+="1";
-            }else{
-                binarioResultante+="0";
+        String binarioPrin = getBinarioValorRegistro(registroPrincipal);
+        String binarioImm = Integer.toBinaryString(valorImm);
+        binarioImm = completarBinario16B(binarioImm);
+        String binarioResultante = "";
+        for (int i = 0; i < 16; i++) {
+            if (binarioPrin.charAt(i) == '1' || binarioImm.charAt(i) == '1') {
+                binarioResultante += "1";
+            } else {
+                binarioResultante += "0";
             }
         }
         setBinarioValorRegistro(registroDestino, binarioResultante);
     }
 
-    public void operacionNot(String registroDestino, String registroPrincipal){
-        String binarioPrin=getBinarioValorRegistro(registroPrincipal);
-        String binarioNot="";
-        for(int i=0;i<16;i++){
-            if(binarioPrin.charAt(i)=='1'){
-                binarioNot+=0;
-            }else{ // si es cero
-                binarioNot+=1;
+    public void operacionNot(String registroDestino, String registroPrincipal) {
+        String binarioPrin = getBinarioValorRegistro(registroPrincipal);
+        String binarioNot = "";
+        for (int i = 0; i < 16; i++) {
+            if (binarioPrin.charAt(i) == '1') {
+                binarioNot += 0;
+            } else { // si es cero
+                binarioNot += 1;
             }
         }
         setBinarioValorRegistro(registroDestino, binarioNot);
     }
 
-    public void operacionNot(String registroDestino, int valorImm){
-        // recordar que el binario del valor imm no necesariamente tiene los 16B que estamos
+    public void operacionNot(String registroDestino, int valorImm) {
+        // recordar que el binario del valor imm no necesariamente tiene los 16B que
+        // estamos
         // trabajando, por tanto, hay que completarlo antes de realizar la operación NOT
-        String binarioImm=completarBinario16B(Integer.toBinaryString(valorImm));
-        String binarioNot="";
-        for(int i=0;i<16;i++){
-            if(binarioImm.charAt(i)=='1'){
-                binarioNot+=0;
-            }else{ // si es cero
-                binarioNot+=1;
+        String binarioImm = completarBinario16B(Integer.toBinaryString(valorImm));
+        String binarioNot = "";
+        for (int i = 0; i < 16; i++) {
+            if (binarioImm.charAt(i) == '1') {
+                binarioNot += 0;
+            } else { // si es cero
+                binarioNot += 1;
             }
         }
         setBinarioValorRegistro(registroDestino, binarioNot);
@@ -343,6 +368,113 @@ public class Arquitectura {
     public void operacionMov(String registroDestino, int Imm) {
         setValorRegistro(registroDestino, Imm);
     }
+
+    /* ACÁ IRIÁN OPERACIONES LSL,LSR,ASR */
+
+    public void operacionLsl(String registroDestino, String registroPrincipal, String registroSecundario) {
+        if (getValorRegistro(registroSecundario) >= 16) {
+            setBinarioValorRegistro(registroDestino, "0000000000000000");
+        } else {
+            String binarioPrin = getBinarioValorRegistro(registroPrincipal);
+            String nuevoBinario = binarioPrin.substring(getValorRegistro(registroSecundario), binarioPrin.length());
+            for (int i = 0; i < getValorRegistro(registroSecundario); i++) {
+                nuevoBinario += "0";
+            }
+            setBinarioValorRegistro(registroDestino, nuevoBinario);
+        }
+    }
+
+    public void operacionLsl(String registroDestino, String registroPrincipal, int valorImm) {
+        if (valorImm >= 16) {
+            setBinarioValorRegistro(registroDestino, "0000000000000000");
+        } else {
+            String binarioPrin = getBinarioValorRegistro(registroPrincipal);
+            String nuevoBinario = binarioPrin.substring(valorImm, binarioPrin.length());
+            for (int i = 0; i < valorImm; i++) {
+                nuevoBinario += "0";
+            }
+            setBinarioValorRegistro(registroDestino, nuevoBinario);
+        }
+    }
+
+    public void operacionLsr(String registroDestino, String registroPrincipal, String registroSecundario) {
+        if (getValorRegistro(registroSecundario) >= 16) {
+            setBinarioValorRegistro(registroDestino, "0000000000000000");
+        } else {
+            String binarioPrin = getBinarioValorRegistro(registroPrincipal);
+            String nuevoBinario = "";
+            for (int i = 0; i < getValorRegistro(registroSecundario); i++) {
+                nuevoBinario += "0";
+            }
+            nuevoBinario += binarioPrin.substring(0, binarioPrin.length() - getValorRegistro(registroSecundario));
+
+            setBinarioValorRegistro(registroDestino, nuevoBinario);
+        }
+    }
+
+    public void operacionLsr(String registroDestino, String registroPrincipal, int valorImm) {
+        if (valorImm >= 16) {
+            setBinarioValorRegistro(registroDestino, "0000000000000000");
+        } else {
+            String binarioPrin = getBinarioValorRegistro(registroPrincipal);
+            String nuevoBinario = "";
+            for (int i = 0; i < valorImm; i++) {
+                nuevoBinario += "0";
+            }
+            nuevoBinario += binarioPrin.substring(0, binarioPrin.length() - valorImm);
+
+            setBinarioValorRegistro(registroDestino, nuevoBinario);
+        }
+    }
+
+    public void operacionAsr(String registroDestino, String registroPrincipal, String registroSecundario) {
+        if (getValorRegistro(registroSecundario) >= 16) {
+            String binarioPrin = getBinarioValorRegistro(registroPrincipal);
+            if (binarioPrin.charAt(0) == '0') {
+                setBinarioValorRegistro(registroDestino, "0000000000000000");
+            } else {
+                setBinarioValorRegistro(registroDestino, "1111111111111111");
+            }
+
+        } else {
+            String binarioPrin = getBinarioValorRegistro(registroPrincipal);
+            String nuevoBinario = "";
+            for (int i = 0; i < getValorRegistro(registroSecundario); i++) {
+                if (binarioPrin.charAt(0) == '0') {
+                    nuevoBinario += "0";
+                } else {
+                    nuevoBinario += "1";
+                }
+            }
+            nuevoBinario += binarioPrin.substring(0, binarioPrin.length() - getValorRegistro(registroSecundario));
+            setBinarioValorRegistro(registroDestino, nuevoBinario);
+        }
+    }
+
+    public void operacionAsr(String registroDestino, String registroPrincipal, int valorImm) {
+        if (valorImm >= 16) {
+            String binarioPrin = getBinarioValorRegistro(registroPrincipal);
+            if (binarioPrin.charAt(0) == '0') {
+                setBinarioValorRegistro(registroDestino, "0000000000000000");
+            } else {
+                setBinarioValorRegistro(registroDestino, "1111111111111111");
+            }
+
+        } else {
+            String binarioPrin = getBinarioValorRegistro(registroPrincipal);
+            String nuevoBinario = "";
+            for (int i = 0; i < valorImm; i++) {
+                if (binarioPrin.charAt(0) == '0') {
+                    nuevoBinario += "0";
+                } else {
+                    nuevoBinario += "1";
+                }
+            }
+            nuevoBinario += binarioPrin.substring(0, binarioPrin.length() - valorImm);
+            setBinarioValorRegistro(registroDestino, nuevoBinario);
+        }
+    }
+
     /*
      * ----------------------------------- FIN
      * OPERACIONES----------------------------------------------
@@ -373,7 +505,8 @@ public class Arquitectura {
     }
 
     /*
-     * Metodo para setear un binario en el valor (númerico) en un registro según su id
+     * Metodo para setear un binario en el valor (númerico) en un registro según su
+     * id
      */
     public void setBinarioValorRegistro(String id, String binario) {
         for (Registro registro : registros) {
@@ -382,7 +515,6 @@ public class Arquitectura {
             }
         }
     }
-
 
     /*
      * Metodo para setear un valor en un registro según su id
@@ -403,16 +535,16 @@ public class Arquitectura {
         }
     }
 
-    public String completarBinario16B(String binario){
+    public String completarBinario16B(String binario) {
         int largoBinario = binario.length();
-        if(largoBinario==16){
+        if (largoBinario == 16) {
             return binario;
-        }else{
-            String binarioCorrecto="";
-            for(int i=0;i<(16-largoBinario);i++){
-                binarioCorrecto+="0";
+        } else {
+            String binarioCorrecto = "";
+            for (int i = 0; i < (16 - largoBinario); i++) {
+                binarioCorrecto += "0";
             }
-            binarioCorrecto+=binario;
+            binarioCorrecto += binario;
             return binarioCorrecto;
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        Arquitectura arq=new Arquitectura();
+        Arquitectura arq = new Arquitectura();
 
-        String nombreArchivo="archivoPrueba1";
-        Archivo archivo1=new Archivo(nombreArchivo);
+        String nombreArchivo = "archivoPrueba2";
+        Archivo archivo1 = new Archivo(nombreArchivo);
         archivo1.leerArchivo();
 
-        if(archivo1.getFormatoCorrecto()){
+        if (archivo1.getFormatoCorrecto()) {
             System.out.println("Archivo en formato correcto");
             arq.ejecutarInstrucciones(archivo1.getInstrucciones());
 
-        }else{
-            System.out.println("El formato del archivo es incorrecto, por favor ve en la carpeta DEBUG_ARchivosDePrueba \n" +
-                    "el archivo: DEBUG_"+nombreArchivo);
+        } else {
+            System.out.println(
+                    "El formato del archivo es incorrecto, por favor ve en la carpeta DEBUG_ARchivosDePrueba \n" +
+                            "el archivo: DEBUG_" + nombreArchivo);
         }
     }
 }
-
